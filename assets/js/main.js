a86700f7af3a7275b22d149aa69683867edec199
Sebastian Vega <vegaarias96@gmail.com>
2024-02-26 14:58:17 -0300

    Se terminan de ordenar los archivos CSS y JS, se hace revision de HTML para que se vea ordenado

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let messageInput = $('#messageFormControlTextarea')
 let submitButton = $('#sendButton')
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('a.nav-link').on('click', function (e) {
         if (this.hash !== '') {
@@ -22,17 +22,17 @@ $(document).ready(function(){
         }
     });
 
-    submitButton.click(function(){
+    submitButton.click(function () {
 
 
-        if(nameInput.val().trim().length > 0 && topicInput.val().trim().length > 0 && messageInput.val().trim().length > 0 ){
+        if (nameInput.val().trim().length > 0 && topicInput.val().trim().length > 0 && messageInput.val().trim().length > 0) {
             nameInput.val("");
             topicInput.val("");
             messageInput.val("");
             submitButton.val("");
 
             alert('El mensaje a sido enviado.')
-        }else{
+        } else {
             alert('ERROR: Ingrese dato en todos los cuadros de texto!')
         }
     })
